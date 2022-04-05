98e9a42e22babf177667f55a58738b222cf7c3ae
timjini <hatim.jini@gmail.com>
2022-04-05 11:32:44 +0300

    update

--- app/models/demand.rb
+++
@@ -1,6 +0,0 @@
-class Demand < ApplicationRecord
-    belongs_to :user
-
-    geocoded_by :address
-    after_validation :geocode, if: :address_changed?
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :demands, dependent: :delete_all
+  has_many :asks, dependent: :delete_all
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
