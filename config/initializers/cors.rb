55e85644219173f571ee937e0a73af8d68683b41
timjini <hatim.jini@gmail.com>
2022-05-16 15:32:36 +0300

    origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -25,6 +25,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       origins "https://auth-app-rails-back.herokuapp.com"
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
+
+    allow do
+      origins "https://hidden-eyrie-18402.herokuapp.com/"
+      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
     
     allow do
       origins 'https://aid-app.netlify.app'
