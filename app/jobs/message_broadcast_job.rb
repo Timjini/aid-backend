b4cac1db8c4362a56f32a1ebd7a350d8b212e06a
timjini <hatim.jini@gmail.com>
2022-05-16 15:35:41 +0300

    update

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,17 +1,17 @@
-# class MessageBroadcastJob < ApplicationJob
-#     queue_as :default
+class MessageBroadcastJob < ApplicationJob
+    queue_as :default
   
-#     def perform(message)
-#       payload = {
-#         room_id: message.conversation.id,
-#         content: message.content,
-#         sender: message.sender,
-#         participants: message.conversation.users.collect(&:id)
-#       }
-#       ActionCable.server.broadcast(build_room_id(message.conversation.id), payload)
-#     end
+    def perform(message)
+      payload = {
+        room_id: message.conversation.id,
+        content: message.content,
+        sender: message.sender,
+        participants: message.conversation.users.collect(&:id)
+      }
+      ActionCable.server.broadcast(build_room_id(message.conversation.id), payload)
+    end
     
-#     def build_room_id(id)
-#       "ChatRoom-#{id}"
-#     end
-#   end
+    def build_room_id(id)
+      "ChatRoom-#{id}"
+    end
+  end
