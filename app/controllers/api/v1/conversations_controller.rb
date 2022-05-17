58c51533cae05e3ff6d6fbf7c07bef010aae9bb6
timjini <hatim.jini@gmail.com>
2022-05-17 17:02:43 +0300

    conversation controller

--- app/controllers/api/v1/conversations_controller.rb
+++ app/controllers/api/v1/conversations_controller.rb
@@ -1,28 +1,28 @@
-# class Api::V1::ConversationsController < Api::V1::BaseController
+class Api::V1::ConversationsController < Api::V1::BaseController
 
-#     def index 
-#         @conversations = current_user.mailbox.conversations
-#          #render json: @conversation
+    def index 
+        @conversations = current_user.mailbox.conversations
+         #render json: @conversation
 
-#     end
+    end
     
-#     def show
-#         @conversations = current_user.mailbox.conversations.find(params[:id])
-#         render json: @conversations
-#     end
+    def show
+        @conversations = current_user.mailbox.conversations.find(params[:id])
+        render json: @conversations
+    end
 
-#     def create
-#         recipient = User.find(params[:user_id])
-#         receipt = current_user.send_message(recipient, params[:body], params[:subject])
-#         redirect_to conversation_path(receipt.conversation)
-#     end
+    def create
+        recipient = User.find(params[:user_id])
+        receipt = current_user.send_message(recipient, params[:body], params[:subject])
+        redirect_to conversation_path(receipt.conversation)
+    end
 
-#     def new
-#         @recipients = User.all - [current_user]
+    def new
+        @recipients = User.all - [current_user]
 
-#     end
+    end
 
 
 
-# end
+end
 
