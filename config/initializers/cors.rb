4cab76976a4070c28d3762732e7e0a45333bb9f8
timjini <hatim.jini@gmail.com>
2022-11-08 10:30:03 +0300

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,7 +15,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # end
     
     allow do
-      origins 'https://aid-app.netlify.ap'
+      origins 'https://aid-app.netlify.app'
   
       resource 'https://aid--backend.herokuapp.com',
       headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
