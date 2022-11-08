89d927465557e2672f551c435ffff18f1a936031
timjini <hatim.jini@gmail.com>
2022-11-08 10:12:25 +0300

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,21 +4,20 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
   
-    allow do
-      origins "*"
-      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
+    # allow do
+    #   origins "*"
+    #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    # end
 
-    allow do
-      origins "https://aid--backend.herokuapp.com"
-      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
+    # allow do
+    #   origins "https://aid--backend.herokuapp.com"
+    #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    # end
     
     allow do
       origins 'https://aid-app.netlify.ap'
   
       resource 'https://aid--backend.herokuapp.com',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
   end
