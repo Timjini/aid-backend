7953876138bde8d13276f7cb3e9c1aac133314ca
timjini <hatim.jini@gmail.com>
2022-11-08 11:37:34 +0300

    table

--- db/migrate/20221108083707_drop_user_conversations.rb
+++ db/migrate/20221108083707_drop_user_conversations.rb
@@ -1,4 +1,5 @@
 class DropUserConversations < ActiveRecord::Migration[6.1]
   def change
+    drop_table :user_conversations
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_07_235414) do
+ActiveRecord::Schema.define(version: 2022_11_08_083707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,15 +70,6 @@ ActiveRecord::Schema.define(version: 2022_11_07_235414) do
     t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
-  create_table "user_conversations", force: :cascade do |t|
-    t.bigint "conversation_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["conversation_id"], name: "index_user_conversations_on_conversation_id"
-    t.index ["user_id"], name: "index_user_conversations_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -107,6 +98,4 @@ ActiveRecord::Schema.define(version: 2022_11_07_235414) do
   add_foreign_key "requests", "users"
   add_foreign_key "tweets", "rooms"
   add_foreign_key "tweets", "users"
-  add_foreign_key "user_conversations", "conversations"
-  add_foreign_key "user_conversations", "users"
 end
