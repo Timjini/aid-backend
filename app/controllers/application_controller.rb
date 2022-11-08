2a4f04455fe1b7d96a6e2a5f5aed99a9e37b83f9
timjini <hatim.jini@gmail.com>
2022-11-08 10:28:52 +0300

    cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::API
+  protect_from_forgery
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
     before_action :configure_permitted_parameters,
     if: :devise_controller?
       protected
@@ -16,4 +19,21 @@ class ApplicationController < ActionController::API
           redirect_to root_path, status: :forbidden, alert: "Unauthorized Access!"
         end
       end
+
+      def cors_set_access_control_headers
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+        headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+        headers['Access-Control-Max-Age'] = "1728000"
+    end
+    
+    def cors_preflight_check
+      if request.method == "OPTIONS"
+        headers['Access-Control-Allow-Origin'] = 'http://localhost'
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+        headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+        headers['Access-Control-Max-Age'] = '1728000'
+        render :text => '', :content_type => 'text/plain'
+      end
+    end
 end
