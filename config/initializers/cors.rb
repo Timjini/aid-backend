a5ecea5481b1ab071bf9a76a90101cdcfd990a14
timjini <hatim.jini@gmail.com>
2022-11-19 10:18:05 +0300

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,18 +1,20 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    # allow do
-    #   origins "http://localhost:3000"
-    #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    # end
-  
-    # allow do
-    #   origins "https://aid-app.netlify.app"
-    #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    # end
+  allow do
+    origins "http://localhost:3000"
 
-    # allow do
-    #   origins "https://aid--backend.herokuapp.com"
-    #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    # ends
+    resource "https://fierce-badlands-13020.herokuapp.com",
+    headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+  
+    allow do
+      origins "https://aid-app.netlify.app"
+      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
+
+    allow do
+      origins "https://fierce-badlands-13020.herokuapp.com"
+      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    ends
     
     allow do
       origins "https://aid-app.netlify.app"
