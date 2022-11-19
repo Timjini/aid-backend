2ac600f7f96764f2819703db09404606d58e9b4b
timjini <hatim.jini@gmail.com>
2022-11-19 10:23:54 +0300

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,19 +2,19 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000"
 
-    resource "https://fierce-badlands-13020.herokuapp.com",
+    resource "*",
     headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
   
-    allow do
-      origins "https://aid-app.netlify.app"
-      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
+    # allow do
+    #   origins "https://aid-app.netlify.app"
+    #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    # end
 
-    allow do
-      origins "https://fierce-badlands-13020.herokuapp.com"
-      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    ends
+    # allow do
+    #   origins "https://aid--backend.herokuapp.com"
+    #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    # ends
     
     allow do
       origins "https://aid-app.netlify.app"
