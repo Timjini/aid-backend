0dc5dff855eac1997280ef72818cb3ca02dd4d62
timjini <hatim.jini@gmail.com>
2022-12-04 21:40:09 +0300

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,12 @@
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "http://localhost:3000"
-#     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-#   end
+config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+  allow do
+    origins "http://localhost:3000"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
 
-#   allow do
-#     origins "https://aid-app.netlify.app"
-#     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-#   end
+  allow do
+    origins "https://aid-app.netlify.app"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
   
-# end
+end
