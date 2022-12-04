68d7c48dc2c1fb891730fcfbe4db6892dd7d67be
timjini <hatim.jini@gmail.com>
2022-12-04 20:02:10 +0300

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,10 +11,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     # end
 
-    # allow do
-    #   origins "https://aid--backend.herokuapp.com"
-    #   resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    # ends
+    allow do
+      origins "https://aid--backend.herokuapp.com"
+      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    ends
     
     allow do
       origins "https://aid-app.netlify.app"
