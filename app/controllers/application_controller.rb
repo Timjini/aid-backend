20abbedbb1290d89c779296f7f05a4a27ad371ec
timjini <hatim.jini@gmail.com>
2022-12-04 20:48:07 +0300

    cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
-  protect_from_forgery
-  before_filter :cors_preflight_check
-  after_filter :cors_set_access_control_headers
+  # protect_from_forgery
+  # before_filter :cors_preflight_check
+  # after_filter :cors_set_access_control_headers
     before_action :configure_permitted_parameters,
     if: :devise_controller?
       protected
@@ -20,20 +20,20 @@ class ApplicationController < ActionController::API
         end
       end
 
-      def cors_set_access_control_headers
-        headers['Access-Control-Allow-Origin'] = '*'
-        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-        headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
-        headers['Access-Control-Max-Age'] = "1728000"
-    end
+    #   def cors_set_access_control_headers
+    #     headers['Access-Control-Allow-Origin'] = '*'
+    #     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    #     headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+    #     headers['Access-Control-Max-Age'] = "1728000"
+    # end
     
-    def cors_preflight_check
-      if request.method == "OPTIONS"
-        headers['Access-Control-Allow-Origin'] = 'http://localhost'
-        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-        headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
-        headers['Access-Control-Max-Age'] = '1728000'
-        render :text => '', :content_type => 'text/plain'
-      end
-    end
+    # def cors_preflight_check
+    #   if request.method == "OPTIONS"
+    #     headers['Access-Control-Allow-Origin'] = 'http://localhost'
+    #     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    #     headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+    #     headers['Access-Control-Max-Age'] = '1728000'
+    #     render :text => '', :content_type => 'text/plain'
+    #   end
+    # end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,18 @@
-config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
   allow do
-    origins 'https://aid-app.netlify.app'
+    origins "https://aid-app.netlify.app"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+  
+  allow do
+    origins "https://aid-app.netlify.app"
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    resource "*",
+    headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
