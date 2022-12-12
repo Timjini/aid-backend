fefe01dc272e2dfaa520d6f8df740c6862120bfb
timjini <hatim.jini@gmail.com>
2022-12-12 15:02:29 +0300

    biker

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_12_112315) do
+ActiveRecord::Schema.define(version: 2022_12_12_120115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,14 +44,14 @@ ActiveRecord::Schema.define(version: 2022_12_12_112315) do
   end
 
   create_table "bikers", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.string "city_of_origin"
     t.string "state_of_origin"
     t.float "latitude"
     t.float "longitude"
-    t.string "first_name"
-    t.string "last_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "conversations", force: :cascade do |t|
