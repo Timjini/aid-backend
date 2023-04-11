cf930b4cbc0984c7849f6341a50d3de0c92ce1bb
Timjini <hatim.jini@gmail.com>
2023-04-11 21:29:10 +0300

    cors back to origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::API
-    before_action :set_cors_headers
     before_action :configure_permitted_parameters,
     if: :devise_controller?
       protected
@@ -18,10 +17,5 @@ class ApplicationController < ActionController::API
         end
       end
 
-      def set_cors_headers
-        headers['Access-Control-Allow-Origin'] = 'https://bikerace-oc.netlify.app/'
-        headers['Access-Control-Allow-Headers'] = 'Content-Type'
-        headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-      end
 
 end
